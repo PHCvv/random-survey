00de33ab427be90d1df390324237f8ba6dca8c64
PHCvv <pinhsuan.chiu@gmail.com>
2024-11-07 12:47:45 +0800

    Update url.js

--- url.js
+++ url.js
@@ -17,7 +17,6 @@ const urls = [
 	'https://forms.fillout.com/t/atYZszXYw2us',
 	'https://forms.fillout.com/t/dcJNGsYAnous',
 	'https://forms.fillout.com/t/dcJNGsYAnous',
-	'https://forms.fillout.com/t/aKrdgWoC6aus',
 	'https://forms.fillout.com/t/279MvePE18us',
 	'https://forms.fillout.com/t/5PJY28yroGus',
 	'https://forms.fillout.com/t/fsvg97D3zuus',
